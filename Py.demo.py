820513a50d275a3a12c25e6428ab164679ff9aea
Franks <franksam1@hotmail.co.uk>
2023-01-15 11:53:29 +0000

    Commit

---
+++ Py.demo.py
@@ -0,0 +1,26 @@
+
+x = 5
+print("Hello")
+
+
+def hobby():
+    print("Do good to others as you would want done to you.")
+    print("I sleep all night and I work all day.")
+    print("The earth is a wonderful piece of rock")
+
+
+print("Yo")
+hobby()
+x = x + 2
+print(x)
+
+n = 5
+
+friends = {"Jojo", "Moses", "Sue"}
+for friend in friends:
+    print("Happy New Year:", friend)
+print("done")
+
+
+t = [9, 41, 12, 3, 74, 15]
+print(t)
